d3c785365c2f3f8b1f1fb0cfc28701eae6b29982
Jibon Krishna Roy <mailme.jibon@gmail.com>
2024-05-06 13:34:23 +0600

    toast event update #2

--- react-image-link-generator-frontend/src/components/dropzone/dropzone.jsx
+++ react-image-link-generator-frontend/src/components/dropzone/dropzone.jsx
@@ -76,7 +76,7 @@ export default function MyDropzone() {
               description: Date.now(),
               action: {
                 label: "Try again",
-                onClick: () => console.log("Undo"),
+                onClick: () => console.log("Notification clicked"),
               },
             });
           }
